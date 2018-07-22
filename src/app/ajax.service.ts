1b87272a8cdb22399edc1ef4bbb7c524cd275307
swb4412001265 <swb4412001265@gmail.com>
2018-07-22 00:51:02 -0400

    Add IMG

--- src/app/ajax.service.ts
+++ src/app/ajax.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-// import { catchError, map, tap } from 'rxjs/operators';
 import { Book } from './Book';
 
 @Injectable({
